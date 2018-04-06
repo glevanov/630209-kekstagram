8e4e5de5c8b6f3388e8cde8f0c1e48f788dd356f
Grigoriy Levanov <glevanov@gmail.com>
2018-04-06 21:24:13 +0300

    Завершает создания массива с фотографиями

--- js/pictures.js
+++ js/pictures.js
@@ -1,1 +1,39 @@
 'use strict';
+
+var picturesArray = [];
+var PICTURES_QUANTITY = 25;
+var PICTURES_LIKES_MIN = 15;
+var PICTURES_LIKES_MAX = 250;
+var PICTURES_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var PICTURES_DESCRIPTION = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
+
+function getRandomInteger(min, max) {
+  return Math.floor(
+      Math.random() * (max - min + 1) + min
+  );
+}
+
+for (var i = 0; i < PICTURES_QUANTITY; i++) {
+  var picElement = {
+    url: 'photos/' + i + '.jpg',
+    likes: getRandomInteger(PICTURES_LIKES_MIN, PICTURES_LIKES_MAX),
+    comments: PICTURES_COMMENTS[getRandomInteger(0, PICTURES_COMMENTS.length - 1)],
+    description: PICTURES_DESCRIPTION[getRandomInteger(0, PICTURES_DESCRIPTION.length - 1)]
+  };
+
+  picturesArray.push(picElement);
+}
