773e4e18e33c833ef8c4b3cb14dcf69e97d75e39
Grigoriy Levanov <glevanov@gmail.com>
2018-04-07 11:58:27 +0300

    Добавляет заготовку блока отрисовки картинок по шаблону

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Здесь генерируем тестовые данные для случайных пользовательских картинок
 var picturesArray = [];
 var PICTURES_QUANTITY = 25;
 var PICTURES_LIKES_MIN = 15;
@@ -46,6 +46,7 @@ function getCommentsArray() {
   return commentsArray;
 }
 
+// Наполняем массив тестовых данных
 function populatePicturesArray() {
   for (var i = 0; i < PICTURES_QUANTITY; i++) {
     var picElement = {
@@ -61,4 +62,12 @@ function populatePicturesArray() {
 
 populatePicturesArray();
 
-var pictureTemplate = document.querySelector('#picture');
+// Отрисовываем случайные картинки с использованием тестовых данных
+var pictureTemplate = document.querySelector('#picture').content;
+var picturesElement = document.querySelector('.pictures');
+
+for (var k = 0; k < PICTURES_QUANTITY; k++) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  picturesElement.appendChild(pictureElement);
+}
