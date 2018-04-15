d65464147f845ddeba91a677b1bf238ffbbcff93
Grigoriy Levanov <glevanov@gmail.com>
2018-04-15 17:23:59 +0300

    Реализует большую часть модуля кроме вытаскивания комментариев по клику

--- js/pictures.js
+++ js/pictures.js
@@ -4,6 +4,8 @@
 var picturesArray = [];
 
 // #02# Константы
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 var PICTURES_QUANTITY = 25;
 var PICTURES_LIKES_MIN = 15;
 var PICTURES_LIKES_MAX = 250;
@@ -24,6 +26,15 @@ var PICTURES_DESCRIPTION = [
   'Вот это тачка!'
 ];
 // #03# DOM переменные
+var bigPicture = document.querySelector('.big-picture');
+var bigPictureCancelButton = bigPicture.querySelector('.big-picture__cancel');
+var uploadSection = document.querySelector('.img-upload');
+var uploadOverlay = uploadSection.querySelector('.img-upload__overlay');
+var uploadFileInput = uploadSection.querySelector('#upload-file');
+var uploadCancelButton = uploadSection.querySelector('#upload-cancel');
+var slider = document.querySelector('.scale');
+var sliderPin = slider.querySelector('.scale__pin');
+var sliderLine = slider.querySelector('.scale__line');
 
 // #04# Объявление функций
 function getRandomInteger(min, max) {
@@ -83,6 +94,18 @@ function outputPictures() {
   }
 }
 
+function appendPicturesEventListeners() {
+  var pictures = document.querySelectorAll('.picture__link');
+  for (var i = 0; i < PICTURES_QUANTITY; i++) {
+    pictures[i].addEventListener('click', onPictureClick);
+  }
+}
+
+function onPictureClick(evt) {
+  outputBigPicture(evt);
+  addEscListener();
+}
+
 function displayHiddenElement(hiddenElement) {
   hiddenElement.classList.remove('hidden');
 }
@@ -92,21 +115,23 @@ function hideElement(visibleElement) {
 }
 
 // Показывает большую картинку и заполняет её данными
-function outputBigPicture() {
+function outputBigPicture(evt) {
   var commentsIndex;
 
-  var COMMENTS_QUANTITY = getRandomInteger(1, 2);
-
-  var bigPicture = document.querySelector('.big-picture');
   var commentsSection = bigPicture.querySelector('.social__comments');
+  var currentElement = evt.currentTarget;
+  var currentImage = currentElement.querySelector('img');
+  var CURRENT_IMAGE_SRC = currentImage.attributes[1].value;
+  var CURRENT_LIKES = currentElement.querySelector('.picture__stat--likes').firstChild.data;
+  var CURRENT_COMMENTS = Number(currentElement.querySelector('.picture__stat--comments').firstChild.data);
 
-  // Генерирует от одного до двух случайных комментария для большой картинки
+  // Генерирует комментарии для большой картинки
   // ...сначала разметку
   function getBigPictureCommentsMarkup() {
     var markup = '';
     var markupElement = '';
 
-    for (commentsIndex = 0; commentsIndex < COMMENTS_QUANTITY; commentsIndex++) {
+    for (commentsIndex = 0; commentsIndex < CURRENT_COMMENTS; commentsIndex++) {
       markupElement = '<li class="social__comment social__comment--text">\n' +
         '  <img class="social__picture" src="img/avatar-' + getRandomInteger(1, 6) + '.svg" \n' +
         '    alt="Аватар комментатора фотографии" \n' +
@@ -119,15 +144,15 @@ function outputBigPicture() {
   }
   // ...потом текст комментария
   function getBigPictureCommentsContent() {
-    for (commentsIndex = 0; commentsIndex < COMMENTS_QUANTITY; commentsIndex++) {
+    for (commentsIndex = 0; commentsIndex < CURRENT_COMMENTS; commentsIndex++) {
       commentsSection.childNodes.item(commentsIndex).insertAdjacentText('beforeend', getComment());
     }
   }
 
   // Показывает большую картинку и вставляет данные
-  // displayHiddenElement(bigPicture);
-  bigPicture.querySelector('.big-picture__img img').src = picturesArray[0].url;
-  bigPicture.querySelector('.likes-count').textContent = picturesArray[0].likes;
+  displayHiddenElement(bigPicture);
+  bigPicture.querySelector('.big-picture__img img').src = CURRENT_IMAGE_SRC;
+  bigPicture.querySelector('.likes-count').textContent = CURRENT_LIKES;
   bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
   bigPicture.querySelector('.social__comment-loadmore').classList.add('visually-hidden');
   // Обнуляет блок комментариев и вставляет случайные комментарии
@@ -137,34 +162,46 @@ function outputBigPicture() {
   getBigPictureCommentsContent();
 }
 
-populatePicturesArray();
-outputPictures();
-outputBigPicture();
-
-// Блок про загрузку фото
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
-
-var uploadSection = document.querySelector('.img-upload');
-var uploadOverlay = uploadSection.querySelector('.img-upload__overlay');
-var uploadFileInput = uploadSection.querySelector('#upload-file');
-var uploadCancelButton = uploadSection.querySelector('#upload-cancel');
-
 function closeUpload() {
   hideElement(uploadOverlay);
   uploadOverlay.value = '';
-  document.removeEventListener('keydown', onEscPress);
+  removeEscListener();
+}
+
+function closeBigPicture() {
+  hideElement(bigPicture);
+  removeEscListener();
 }
 
 function onEscPress(evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closeUpload();
+    closeBigPicture();
   }
 }
 
+function addEscListener() {
+  document.addEventListener('keydown', onEscPress);
+}
+
+function removeEscListener() {
+  document.removeEventListener('keydown', onEscPress);
+}
+
+function getSliderValue(evt) {
+  var sliderXPosition = evt.clientX;
+  var scaleXLeftPosition = sliderLine.getBoundingClientRect().left;
+  var scaleXRightPosition = sliderLine.getBoundingClientRect().right;
+  var scaleWidth = scaleXRightPosition - scaleXLeftPosition;
+  var sliderRelativePosition = sliderXPosition - scaleXLeftPosition;
+  var sliderValue = Math.round(sliderRelativePosition / scaleWidth * 100) / 100;
+  return sliderValue;
+}
+
+// #05# Разное
 uploadFileInput.addEventListener('change', function () {
   displayHiddenElement(uploadOverlay);
-  document.addEventListener('keydown', onEscPress);
+  addEscListener();
 });
 
 uploadFileInput.addEventListener('keydown', function (evt) {
@@ -176,3 +213,17 @@ uploadFileInput.addEventListener('keydown', function (evt) {
 uploadCancelButton.addEventListener('click', function () {
   closeUpload();
 });
+
+bigPictureCancelButton.addEventListener('click', function () {
+  closeBigPicture();
+});
+
+// Временно - выводит в консоль значение слайдера насыщенности эффекта
+sliderPin.addEventListener('mouseup', function (evt) {
+  console.log(getSliderValue(evt));
+});
+
+// #06# Вызовы функций
+populatePicturesArray();
+outputPictures();
+appendPicturesEventListeners();
